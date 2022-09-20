e42184fd2f43c11b1b83fb9bf2d217c1019ee8bb
Andre Menezes <djdedemenezes@gmail.com>
2022-09-20 13:24:23 -0300

    fixed strong params and extracted sms_code into method

--- app/controllers/twilio_code_confirmations_controller.rb
+++ app/controllers/twilio_code_confirmations_controller.rb
@@ -4,9 +4,8 @@ class TwilioCodeConfirmationsController < ApplicationController
   end
 
   def create
-    sms_code = "#{first_digit_params}#{second_digit_params}#{third_digit_params}#{fourth_digit_params}#{fifth_digit_params}#{sixth_digit_params}"
-
-    verification_check = Twilio::SMS.new(session[:twilio_sid]).verification_check(session[:user_phone_number], sms_code)
+    verification_check = Twilio::SMS.new(session[:twilio_sid])
+                                    .verification_check(session[:user_phone_number], sms_code)
     if verification_check.status == 'approved'
       redirect_to new_user_registration_path
       flash[:notice] = 'Code approved'
@@ -18,10 +17,11 @@ class TwilioCodeConfirmationsController < ApplicationController
 
   private
 
-  %i[first second third fourth fifth sixth].each do |method|
-    method_name = "#{method.to_s}_digit_params"
-    define_method method_name.to_sym do
-      params.require(method).permit(method)[method]
-    end
+  def sms_code
+    confirmation_code_params.values.join
+  end
+
+  def confirmation_code_params
+    params.require(:confirmation_code).permit(:first, :second, :third, :fourth, :fifth, :sixth)
   end
 end
