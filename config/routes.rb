465740fcfd20023c4185f82d50d4f45dce6bbc59
Andre Menezes <djdedemenezes@gmail.com>
2022-09-21 18:49:41 -0300

    remove /:locale from route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  scope '/:locale' do
-    root to: "pages#home"
-    devise_for :users
+  root to: "pages#home"
+  devise_for :users
 
-    get 'login', to: 'twilio_code_messages#new', as: :login
-    post 'login', to: 'twilio_code_messages#create', as: :create_login
-    get 'confirmation', to: 'twilio_code_confirmations#new', as: :new_sms
-    post 'confirmation', to: 'twilio_code_confirmations#create', as: :confirm_sms
-    get 'profile', to: 'profiles#show', as: :profile
-    resources :doctors, only: %i[index show]
-  end
+  get 'login', to: 'twilio_code_messages#new', as: :login
+  post 'login', to: 'twilio_code_messages#create', as: :create_login
+  get 'confirmation', to: 'twilio_code_confirmations#new', as: :new_sms
+  post 'confirmation', to: 'twilio_code_confirmations#create', as: :confirm_sms
+  get 'profile', to: 'profiles#show', as: :profile
+  resources :doctors, only: %i[index show]
 end
