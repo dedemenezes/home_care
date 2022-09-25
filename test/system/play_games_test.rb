5f067dc3a47eb8e537add374b659c2af705503a8
Andre Menezes <djdedemenezes@gmail.com>
2022-09-25 05:27:32 -0300

    started booking

--- test/system/play_games_test.rb
+++ test/system/play_games_test.rb
@@ -1,24 +1,24 @@
-require "application_system_test_case"
+# require "application_system_test_case"
 
-class PlayGamesTest < ApplicationSystemTestCase
-  test 'answering one question' do
-    @q_one = questions(:why_tests)
-    @q_two = questions(:why_unit_tests)
-    doc = users(:doc)
-    round = rounds(:first)
-    login_as doc
-    visit "/rounds/#{round.id}"
-    assert_text 'Trivia'
-    assert_text questions(:why_tests).content
-    assert_text options(:why_tests_right_option).content
-    assert_text options(:why_tests_b).content
-    assert_text options(:why_tests_c).content
-    assert_text options(:why_tests_d).content
-    choose 'Is important', allow_label_click: true
-    click_button 'Submit'
-    # without sleep it doesn't find the answer 🤔
-    sleep(1)
-    assert_equal 1, Answer.count
-    assert_text questions(:why_unit_tests).content
-  end
-end
+# class PlayGamesTest < ApplicationSystemTestCase
+#   test 'answering one question' do
+#     @q_one = questions(:why_tests)
+#     @q_two = questions(:why_unit_tests)
+#     doc = users(:doc)
+#     round = rounds(:first)
+#     login_as doc
+#     visit "/rounds/#{round.id}"
+#     assert_text 'Trivia'
+#     assert_text questions(:why_tests).content
+#     assert_text options(:why_tests_right_option).content
+#     assert_text options(:why_tests_b).content
+#     assert_text options(:why_tests_c).content
+#     assert_text options(:why_tests_d).content
+#     choose 'Is important', allow_label_click: true
+#     click_button 'Submit'
+#     # without sleep it doesn't find the answer 🤔
+#     sleep(1)
+#     assert_equal 1, Answer.count
+#     assert_text questions(:why_unit_tests).content
+#   end
+# end
