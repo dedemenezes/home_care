99b32637ed0b2e349cedee383092554fb7eab6ab
Andre Menezes <djdedemenezes@gmail.com>
2022-09-27 22:54:43 -0300

    removed binding.pry

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -5,7 +5,6 @@ class RoundsController < ApplicationController
     @question = Question.where(level: @round.level).first
     @question = Question.find(params[:question]) if params[:question].present?
     @answer = Answer.new
-    binding.pry
     authorize @round
   end
 
@@ -25,7 +24,6 @@ class RoundsController < ApplicationController
 
   def score
     @round = Round.find(params[:id])
-    binding.pry
     if @round.correct_answers_count == 5 && !@round.completed?
       @round.completed!
     end
