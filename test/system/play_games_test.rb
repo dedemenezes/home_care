e1544f9f5084d73ccfe59af09b1ccae17462ed15
Andre Menezes <djdedemenezes@gmail.com>
2022-09-29 17:59:40 -0300

    removed answering question test. too confuse

--- test/system/play_games_test.rb
+++ test/system/play_games_test.rb
@@ -1,24 +1,20 @@
 require "application_system_test_case"
 
 class PlayGamesTest < ApplicationSystemTestCase
-  test 'answering one question' do
+  setup do
     @q_one = questions(:brande_new_question_1)
     @q_two = questions(:brande_new_question_2)
-    marty = users(:marty)
-    round = rounds(:brande_new)
-    login_as marty
-    visit "/rounds/#{round.id}"
-    save_screenshot
+    @marty = users(:marty)
+    login_as @marty
+  end
+
+  test 'creating new round' do
+    visit '/games'
     assert_text 'new game'
-    assert_text @q_one.content
-    @q_one.options.each do |opt|
-      assert_text opt.content
-    end
-    choose 'brande_new_question_1', allow_label_click: true
-    click_button 'Submit'
-    # without sleep it doesn't find the answer 🤔
-    # sleep(2)
-    assert_equal 1, round.answers.count(&:correct)
-    assert_text questions(:why_unit_tests).content
+    assert_selector 'h1', text: 'Games'
+    assert_selector 'h2', text: @q_one.game.name
+    assert_selector 'form#new_round', count: 3
+    click_on 'create_new_game'
+    assert_selector 'h2', text: @q_one.content
   end
 end
