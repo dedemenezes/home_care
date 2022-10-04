d9b34c5e505a21bcaadca067f76d2a736da1aa99
Andre Menezes <djdedemenezes@gmail.com>
2022-10-04 20:10:32 -0300

    when no moer questions game is completed

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,8 @@
 class Game < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :rounds
+
+  def completed?(user)
+    rounds.where(user: user, completed: true).count == 5
+  end
 end
