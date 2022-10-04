4d86e7736711eee5b28cba45d81afcdd109040ac
Andre Menezes <djdedemenezes@gmail.com>
2022-10-04 19:40:42 -0300

    removing comments

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -28,10 +28,6 @@ class RoundsController < ApplicationController
       @round.completed!
     end
     if @round.completed? && @round.points_not_given?
-      # won't work because the game can be referenced in many differnet users
-      # a game can have many users and a user will have many games
-      # need to create a join table
-      # @round.game.update(level: @round.game.level += 1)
       current_user.update(points: current_user.points += 10)
       @round.update(points_given: true)
     end
