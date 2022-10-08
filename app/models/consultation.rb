6f00c5f39be3b7aa8a92d4f37068e41e08854186
Andre Menezes <djdedemenezes@gmail.com>
2022-10-08 18:55:34 -0300

    testing error message when creating consultation

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -4,10 +4,17 @@ class Consultation < ApplicationRecord
   validates :location, :starting_at, :price, presence: true
   validates :price, numericality: { only_integer: true, greater_than: 0 }
   validate :starting_at_overlap #, :discount_cannot_be_greater_than_total_value
+  validate :starting_at_not_in_the_past #, :discount_cannot_be_greater_than_total_value
+
+  def starting_at_not_in_the_past
+    if starting_at.present? && starting_at < Date.today
+      errors.add(:starting_at, "can't be in the past")
+    end
+  end
 
   def starting_at_overlap
     if starting_at.present? && starting_at > Date.today
-      if self.class.find_by(starting_at: starting_at)
+      if Consultation.find_by(starting_at: starting_at)
         errors.add(:starting_at, "date is not available")
       end
     end
--- test/system/consultations_test.rb
+++ test/system/consultations_test.rb
@@ -1,9 +1,12 @@
 require 'application_system_test_case'
 
 class BookingsTest < ApplicationSystemTestCase
+  def setup
+    @dede = users(:dede)
+    login_as @dede
+  end
+
   test 'signed in user can create a consultation' do
-    dede = users(:dede)
-    login_as dede
     visit "/doctors/#{users(:doc).id}"
     assert_equal 1, Consultation.all.size
     assert_text users(:doc).email
@@ -13,10 +16,18 @@ class BookingsTest < ApplicationSystemTestCase
     select Date.today.day, from: 'consultation_starting_at_3i'
     select DateTime.now.hour, from: 'consultation_starting_at_4i'
     click_on 'Create Consultation'
-    assert_text dede.email
+    assert_text @dede.email
     assert_text 'Edit profile'
     assert_text 'Consultations'
     assert_equal 2, Consultation.count
-    assert_equal dede, Consultation.last.patient
+    assert_equal @dede, Consultation.last.patient
+  end
+
+  test 'render error message if starting at is in the past' do
+    visit "/doctors/#{users(:doc).id}"
+    select '2021', from: 'consultation_starting_at_1i'
+    click_on 'Create Consultation'
+    sleep(2)
+    assert_text "Starting at can't be in the past"
   end
 end
